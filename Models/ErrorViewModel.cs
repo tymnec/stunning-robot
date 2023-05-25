eccc830a78766cdb3b392f8e88587f38680960b7
Nikhi <Nikhi@DEB>
2023-05-25 11:02:09 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bluetooth_Controller_Application.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
